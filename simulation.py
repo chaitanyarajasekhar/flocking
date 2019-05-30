fa205becd4b1f56c364ec8eebe1a398cb960b76d
Siyu Zhou <siyu.zhou@outlook.com>
2019-05-29 17:54:18 -0700

    Add multiprocessing.

--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import json
 import time
+import multiprocessing
 
 import numpy as np
 
@@ -26,6 +27,50 @@ def random_obstacle(position1, position2, r):
     return Sphere(r, [true_x, true_y], ndim=2)
 
 
+def simulation(_):
+    region = (-100, 100, -100, 100)
+
+    env = Environment2D(region)
+
+    for _ in range(ARGS.boids):
+        agent = Boid(ndim=2, vision=ARGS.vision, size=ARGS.size,
+                     max_speed=10, max_acceleration=20)
+        agent.initialize(np.random.uniform(-80, 80, 2),
+                         np.random.uniform(-15, 15, 2))
+        env.add_agent(agent)
+    for _ in range(ARGS.vicseks):
+        agent = Vicsek(ndim=2, vision=ARGS.vision, size=ARGS.size,
+                       max_speed=10, max_acceleration=20)
+        agent.initialize(np.random.uniform(-80, 80, 2),
+                         np.random.uniform(-15, 15, 2))
+        env.add_agent(agent)
+
+    goal = Goal(np.random.uniform(-40, 40, 2), ndim=2)
+    env.add_goal(goal)
+    # Create a sphere obstacle near segment between avg boids position and goal position.
+    avg_boids_position = np.mean(
+        np.vstack([agent.position for agent in env.population]), axis=0)
+
+    spheres = []
+    for _ in range(ARGS.obstacles):
+        sphere = random_obstacle(avg_boids_position, goal.position, 8)
+        spheres.append(sphere)
+        env.add_obstacle(sphere)
+
+    position_data = []
+    velocity_data = []
+    for _ in range(ARGS.steps):
+        env.update(ARGS.dt)
+        position_data.append([goal.position for goal in env.goals] +
+                             [sphere.position for sphere in spheres] +
+                             [agent.position.copy() for agent in env.population])
+        velocity_data.append([np.zeros(2) for goal in env.goals] +
+                             [np.zeros(2) for sphere in spheres] +
+                             [agent.velocity.copy() for agent in env.population])
+
+    return position_data, velocity_data
+
+
 def main():
     if not os.path.exists(ARGS.save_dir):
         os.makedirs(ARGS.save_dir)
@@ -38,59 +83,29 @@ def main():
     if ARGS.vicseks > 0:
         Vicsek.set_model(model_config["vicsek"])
 
-    region = (-100, 100, -100, 100)
-
+    pool = multiprocessing.Pool(processes=ARGS.processes)
     position_data_all = []
     velocity_data_all = []
 
+    instances = ARGS.instances
+    batch = 100
+
     prev_time = time.time()
-    for i in range(ARGS.instances):
-        if i % 100 == 0:
-            print('Simulation {}/{}... {:.1f}s'.format(i,
-                                                       ARGS.instances, time.time()-prev_time))
-            prev_time = time.time()
+    while instances > 0:
+        n = min(instances, batch)
+        data_pool = pool.map(simulation, range(n))
 
-        env = Environment2D(region)
-        for _ in range(ARGS.boids):
-            agent = Boid(ndim=2, vision=ARGS.vision, size=ARGS.size,
-                         max_speed=10, max_acceleration=20)
-            agent.initialize(np.random.uniform(-80, 80, 2),
-                             np.random.uniform(-15, 15, 2))
-            env.add_agent(agent)
-        for _ in range(ARGS.vicseks):
-            agent = Vicsek(ndim=2, vision=ARGS.vision, size=ARGS.size,
-                           max_speed=10, max_acceleration=20)
-            agent.initialize(np.random.uniform(-80, 80, 2),
-                             np.random.uniform(-15, 15, 2))
-            env.add_agent(agent)
+        position_pool, velocity_pool = zip(*data_pool)
 
-        goal = Goal(np.random.uniform(-40, 40, 2), ndim=2)
-        env.add_goal(goal)
-        # Create a sphere obstacle near segment between avg boids position and goal position.
-        avg_boids_position = np.mean(
-            np.vstack([agent.position for agent in env.population]), axis=0)
+        instances -= n
+        print('Simulation {}/{}... {:.1f}s'.format(ARGS.instances - instances,
+                                                   ARGS.instances, time.time()-prev_time))
+        prev_time = time.time()
 
-        spheres = []
-        for _ in range(ARGS.obstacles):
-            sphere = random_obstacle(avg_boids_position, goal.position, 8)
-            spheres.append(sphere)
-            env.add_obstacle(sphere)
+        position_data_all.extend(position_pool)
+        velocity_data_all.extend(velocity_pool)
 
-        position_data = []
-        velocity_data = []
-        for _ in range(ARGS.steps):
-            env.update(ARGS.dt)
-            position_data.append([goal.position for goal in env.goals] +
-                                 [sphere.position for sphere in spheres] +
-                                 [agent.position.copy() for agent in env.population])
-            velocity_data.append([np.zeros(2) for goal in env.goals] +
-                                 [np.zeros(2) for sphere in spheres] +
-                                 [agent.velocity.copy() for agent in env.population])
-
-        position_data_all.append(position_data)
-        velocity_data_all.append(velocity_data)
-
-    print('Simulations {0}/{0} completed.'.format(ARGS.instances))
+    # print('Simulations {0}/{0} completed.'.format(ARGS.instances))
 
     np.save(os.path.join(ARGS.save_dir, ARGS.prefix +
                          '_position.npy'), position_data_all)
@@ -123,6 +138,8 @@ if __name__ == '__main__':
                         help='name of the save directory')
     parser.add_argument('--prefix', type=str, default='',
                         help='prefix for save files')
+    parser.add_argument('--processes', type=int, default=1,
+                        help='number of parallel processes')
 
     ARGS = parser.parse_args()
 
